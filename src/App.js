03038cbb77e7e066ae113d61704974d0595e2d86
Joe Morales <38666795+STEEZYDesigns@users.noreply.github.com>
2021-01-24 17:12:38 -0800

    Add files via upload
    Cleaned up after animation implementation. wiping submit entity

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import logo from './logo.svg';
+import {animations} from './animations.js';
 import './App.css';
-// import collapseSubmitDiv from './animations.js';
 
 //global function to trim the whitespace and add anything necessary
 //to the search query to make it able to pass to whatever i use (API or google, bing etc)
@@ -16,11 +16,17 @@ class SearchBar extends Component {
     super(props);
     this.state = { 
       value: 'Water Drop Search Bar by Joe Morales', //this value will be submitted and used to make a search on something.
-      open: false
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.setSearchValue = this.setSearchValue.bind(this);
+  }
+
+  handleAnimations(target) {
+    // const target = target; //take the button
+    const submitField = target.parentElement; //take the div to collapse
+    animations.dissapear(target, submitField); //gibu anime-shon
   }
 
   handleChange(event) {    
@@ -29,38 +35,13 @@ class SearchBar extends Component {
     this.setState({value: value});  
   }
 
-  setSearchValue() {
-    let value = this.state.value;
-    value = trim(value);
-    // console.log()
-    console.log("Will search for: " + this.state.value);
-  }
-
   handleSubmit(event) {
+    this.handleAnimations(event.target);
+
     console.log('A search was submitted: Hooray. I hope this component works soon :)');
     //when we click "Search", we want a concise search query
     this.setSearchValue();
     //below we can provide the animation to close out the submit button.
-
-    this.setState({
-      open: !this.state.open
-    })
-
-    // console.log("event target")
-    // console.log(event.target); // button == submit. thus button HTML object is event.target
-    // const submitField = (event.target).parentElement; //div to collapse
-    //CAN I SET A DELAYED STYLE: DISPLAY NONE ?
-    // submitField.style = "animation: SubmitField linear 0.5s normal; "
-    // submitField.style = "display: none";
-    //transition and onclick?
-
-    const submitField = (event.target).parentElement; 
-    submitField.style = "height: 0px;";
-    event.target.style = `visibility: hidden;
-    opacity: 0;
-    transition: visibility 0s 1s, opacity 1s linear;`;
-
-    console.log((event.target).parentElement);
     
     event.preventDefault(); //look up what this does
   }
@@ -69,6 +50,17 @@ class SearchBar extends Component {
     //here we would replace the searchbar, and submit button once the query
     //has been executed and results have been returned.
   }
+
+  setSearchValue() {
+    let value = this.state.value;
+    value = trim(value);
+    // console.log()
+    console.log("Will search for: " + this.state.value);
+    this.setState({
+      value: value
+    })
+  }
+
 
   render() {
     return (
